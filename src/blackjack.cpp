93945bb81685f374735558965b74e47e324425b7
asxin0 <Lucasxing4069@gmail.com>
2026-05-14 22:10:38 -0400

    Improve daily reward timer display

--- src/blackjack.cpp
+++ src/blackjack.cpp
@@ -31,7 +31,7 @@ void LoadBlackjackFile(int& chips, int& lastDailyCheck) {
 }
 
 void SaveBlackjackFile(int chips, int lastDailyCheck) {
-	std::ofstream file("Blackjack.txt");
+	std::ofstream file("Blackjack.txt");\
 	file << chips << "\n";
 	file << lastDailyCheck;
 	file.close();
@@ -47,16 +47,39 @@ void DailyBlackjackReward(int chips, int lastDailyCheck) {
 	int timeFromDay = currentTime % 86400;
 	int currentDailyCheck = currentTime - timeFromDay;
 
+	char timeMagnitude = 's';
+	int timeRemainder;
+	int nextLoginTime;
+
+	nextLoginTime = 86400 - timeFromDay;
+
+	if (nextLoginTime / 60 >= 1) {
+		nextLoginTime = nextLoginTime / 60;
+		timeMagnitude = 'm';
+
+		if (nextLoginTime / 60 > 1) {
+			timeRemainder = nextLoginTime % 60;
+			nextLoginTime = nextLoginTime / 60;
+			timeMagnitude = 'h';
+		}
+	}
+
 	if (currentDailyCheck >= lastDailyCheck + 86400) {
 		lastDailyCheck = currentDailyCheck;
 		chips = chips + 1500;
 		SaveBlackjackFile(chips, lastDailyCheck);
 
-		std::cout << "Thank you for logging in today! Your daily reward of 1500 chips has been depositied.";
-		std::cout << "Login again in " << 86400 - timeFromDay << " seconds for your next reward.\n\n";
+		std::cout << "Thank you for logging in today! Your daily reward of 1500 chips has been depositied.\n";
 	}
-	else {
-		std::cout << "Login again in " << 86400 - timeFromDay << " seconds for your next reward.\n\n";
+	switch (timeMagnitude) {
+	case 'h':
+		std::cout << "Login again in " << nextLoginTime << timeMagnitude << " " << timeRemainder << "m" << " for your next reward.\n\n";
+		break;
+	case 'm':
+		std::cout << "Login again in " << nextLoginTime << timeMagnitude << " for your next reward.\n\n";
+		break;
+	default:
+		std::cout << "Login again in " << 86400 - timeFromDay << timeMagnitude << " for your next reward.\n\n";
 	}
 }
 
@@ -147,6 +170,7 @@ void BlackjackGame(int& bet, int& totalBalance, int& cardsDrawn, int(&cards)[10]
 	std::vector<int> dealerCards = {};
 	std::random_device rd;
 	std::mt19937 gen(rd());
+
 	int playerTotal = 0;
 	int dealerTotal = 0;
 
